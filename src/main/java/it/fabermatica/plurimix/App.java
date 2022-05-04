8a20efafc535aa1737d0bc67f83124b4e0c89444
Giustino De Vincentiis <g.devincentiis@almaviva.it>
2022-05-04 16:10:13 +0200

    maven project

---
+++ src/main/java/it/fabermatica/plurimix/App.java
@@ -0,0 +1,13 @@
+package it.fabermatica.plurimix;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
